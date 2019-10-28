ea9a5d5fe4b7e5256403f522f24975133bdba79c
Nishant Jaiswal <NISHANJ@amdocs.com>
2019-10-28 13:36:14 +0530

    first commit for currency-echange service

---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+insert into exchange_value(id,currency_from,currency_to,conversion_multiple,port)
+values(1001L,'USD','INR',70,0);
+insert into exchange_value(id,currency_from,currency_to,conversion_multiple,port)
+values(1002L,'EUR','INR',65,0);
+insert into exchange_value(id,currency_from,currency_to,conversion_multiple,port)
+values(1003L,'AUD','INR',25,0);
